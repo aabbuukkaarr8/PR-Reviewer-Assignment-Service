6d5b8bc98c2b0eba4d441f3c697384a8e6f0e665
aabbuukkaarr8 <aabbuukkaarr8928@gmail.com>
2025-11-16 15:07:45 +0300

    finish

--- internal/handler/team/dto.go
+++ internal/handler/team/dto.go
@@ -7,7 +7,7 @@ import (
 type MemberTeam struct {
 	UserID   string `json:"user_id" binding:"required"`
 	Username string `json:"username" binding:"required"`
-	IsActive bool   `json:"is_active" binding:"required"`
+	IsActive bool   `json:"is_active"`
 }
 
 type Team struct {
